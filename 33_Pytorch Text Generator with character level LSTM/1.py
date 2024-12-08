1796ffa5ef79165fceac9276c3ed73677c807119
sanele <shlabisa@sarao.ac.za>
2024-12-08 23:57:57 +0200

    Completed a codefor text generator

--- 33_Pytorch Text Generator with character level LSTM/1.py
+++ 33_Pytorch Text Generator with character level LSTM/1.py
@@ -8,7 +8,7 @@ import string
 import random
 import sys
 import unidecode
-from torch.utils.tensorboard import SummaryWriterr
+from torch.utils.tensorboard import SummaryWriter
 
 # Device configuration
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -18,3 +18,109 @@ all_charecters = string.printable
 n_charecters = len(all_charecters)
 
 # Read large text file
+file = unidecode.unidecode(open('../data/babynames.txt').read())
+
+# Model defination
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, output_size):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+    
+        self.embed = nn.Embedding(input_size, hidden_size)
+        self.lstm = nn.LSTM(hidden_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        out = self.embed(x)
+        out, (hidden, cell) = self.lstm(out.unsqueeze(1), (hidden, cell))
+        out = self.fc(out.reshape(out.shape[0], -1))
+        return out, (hidden, cell)
+    
+    def init_hidden(self, batch_size):
+        hidden = torch.zeros(self.num_layers, batch_size, self.hidden_size).to(device)
+        cell = torch.zeros(self.num_layers, batch_size, self.hidden_size).to(device)
+        return hidden, cell
+    
+
+class Generator():
+    def __init__(self):
+        self.chunk_len = 250
+        self.num_epochs = 500
+        self.batch_size  = 1
+        self.print_every = 50
+        self.hidden_size = 256
+        self.num_layers = 2
+        self.lr = 0.003
+
+    def char_tensor(self, string):
+        tensor = torch.zeros(len(string)).long()
+        for c in range(len(string)):
+            tensor[c] = all_charecters.index[string[c]]
+        return tensor
+
+    def get_random_batch(self):
+        start_idx = random.randint(0, len(file) - self.chunk_len)
+        end_idx = start_idx + self.chunk_len + 1
+        text_str = file[start_idx: end_idx]
+        text_input = torch.zeros(self.batch_size, self.chunk_len)
+        text_target = torch.zero(self.batch_size, self.chunk_len)
+
+        for i in range(self.batch_size):
+            text_input[i, :] = self.char_tensor(text_str[:-1])
+            text_target[i, :] = self.char_tensor(text_str[1:])
+
+        return text_input.long(), text_target.long()
+
+    def generate(self, initial_str='Ab', prediction_len=100, temperature=0.85):
+        hidden, cell = self.rnn.init_hidden(batch_size=self.batch_size)
+        initial_input = self.char_tensor(initial_str)
+        predicted = initial_str
+
+        for p in range(len(initial_str) - 1):
+            _, (hidden, cell) = self.rnn(initial_input[p].view(1).to(device), hidden, cell)
+
+        last_char = initial_input[-1]
+
+        for p in range(len(prediction_len) - 1):
+            output, (hidden, cell) = self.rnn(last_char[p].view(1).to(device), hidden, cell)
+            output_dist = output.view(-1).div(temperature).exp()
+            top_char = torch.multinomial(output_dist, 1)[0]
+            predicted_char = all_charecters[top_char]
+            predicted += predicted_char
+            last_char = self.char_tensor(predicted_char)
+
+        return predicted
+
+
+    def train(self):
+        self.rnn = RNN(n_charecters, self.hidden_size, self.num_layers, n_charecters).to(device)
+
+        optimizer = torch.optim.Adam(self.rnn.parameters(), lr=self.lr)
+        criterion = nn.CrossEntropyLoss()
+        writer = SummaryWriter(f"runs/name0")
+
+        print("=> Starting training...")
+
+        for epoch in range(1, self.num_epochs + 1):
+            inp, target = self.get_random_batch()
+            hidden, cell = self.rnn.init_hidden(batch_size=self.batch_size)
+
+            self.rnn.zero_grad()
+            loss = 0
+            inp = inp.to(device)
+            target = target.to(device)
+
+            for c in range(self.chunk_len):
+                output, (hidden, cell) = self.rnn(inp[:, c], hidden, cell)
+                loss += criterion(output, target[:, c])
+
+            loss.backward()
+            optimizer.step()
+            loss = loss.item() / self.chunk_len
+
+            if epoch % self.print_every == 0:
+                print(f"Loss: {loss}")
+                print(self.generate())
+
+            writer.add_scalar('Training Loss', loss, global_step=epoch)
